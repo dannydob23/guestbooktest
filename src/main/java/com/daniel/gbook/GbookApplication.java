d8d3980206691b1d76bac7325dd271ac7c342eaa
Daniel Connolly <billydan76@gmail.com>
2018-11-14 11:11:06 +0000

    Initial commit

---
+++ src/main/java/com/daniel/gbook/GbookApplication.java
@@ -0,0 +1,12 @@
+package com.daniel.gbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GbookApplication.class, args);
+	}
+}
